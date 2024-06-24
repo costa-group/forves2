f2b13fa9d60d908b732824295366c0ade85704da
Samir Genaim <genaim@gmail.com>
2024-06-24 09:34:13 +0200

    forgot to add a file in the last commit

---
+++ context_facts.v
@@ -0,0 +1,23 @@
+Require Import List.
+Import ListNotations.
+Require Import bbv.Word.
+Require Import Nat. 
+Require Import Coq.NArith.NArith.
+ 
+Require Import FORVES2.octagon.
+Import Octagon.
+
+Require Import FORVES2.constraints.
+Import Constraints.
+
+Require Import FORVES2.context.
+Import Context.
+
+Require Import FORVES2.symbolic_state.
+Import SymbolicState.
+
+
+Module ContextFacts.
+
+
+End ContextFacts.
