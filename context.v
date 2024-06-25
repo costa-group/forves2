6bba4daada89557fdbeaea65df7f91f464263483
Samir Genaim <genaim@gmail.com>
2024-06-25 14:56:04 +0200

    some more context

--- context.v
+++ context.v
@@ -50,6 +50,13 @@ Definition sstack_val_to_cliteral (sv : sstack_val) : option cliteral :=
   | _ => None
   end.
 
+Definition sstack_val_to_cliteral_w_shift (sv : sstack_val) (shift: N) : option cliteral :=
+  match sv with
+  | Val w => Some (C_VAL ((wordToN w) + shift))
+  | InVar n => Some (C_VAR_DELTA n shift)
+  | _ => None
+  end.
+
 Definition chk_eq_wrt_ctx (ctx: ctx_t) (sv1 sv2: sstack_val) :=
   let imp_chkr := imp_checker_fun (ctx_imp_chkr ctx) in
   let cs := ctx_cs ctx in
@@ -72,6 +79,16 @@ Definition chk_lt_wrt_ctx (ctx: ctx_t) (sv1 sv2: sstack_val) :=
 
 Definition chk_neq_wrt_ctx (ctx: ctx_t) (sv1 sv2: sstack_val) :=
   chk_lt_wrt_ctx ctx sv1 sv2 ||   chk_lt_wrt_ctx ctx sv2 sv1.
+
+Definition chk_lt_wshift_wrt_ctx (ctx: ctx_t) (sv1 sv2: sstack_val) (shift: N) :=
+  let imp_chkr := imp_checker_fun (ctx_imp_chkr ctx) in
+  let cs := ctx_cs ctx in
+  let ocl1 := sstack_val_to_cliteral_w_shift sv1 shift in
+  let ocl2 := sstack_val_to_cliteral sv2 in
+  match ocl1, ocl2 with
+  | Some cl1, Some cl2 => imp_chkr cs (C_LT cl1 cl2)
+  | _, _ => false
+  end.
 
 
 End Context.
--- context_facts.v
+++ context_facts.v
@@ -165,7 +165,8 @@ Qed.
         exists v1 v2,
           eval_sstack_val sv1 model mem strg exts maxidx sb ops = Some v1 /\
             eval_sstack_val sv2 model mem strg exts maxidx sb ops = Some v2 /\
-            wlt v1 v2.
+            (wordToN v1 < wordToN v2)%N.
+    Proof.
     intros ctx sv1 sv2 H_chkr model mem strg exts maxidx sb ops H_is_model.
     destruct ctx as [cs [chkr H_chkr_snd]].
     simpl in H_is_model.
@@ -198,11 +199,6 @@ Qed.
       exists val2.
       repeat split; try reflexivity.
 
-      apply lt_wlt.
-
-      apply Nlt_out in H_chkr_snd.
-
-      repeat rewrite wordToN_to_nat in H_chkr_snd.
       apply H_chkr_snd.
 
     (* val = var *)
@@ -229,11 +225,6 @@ Qed.
       exists (model var2).
       repeat split; try reflexivity.
 
-      apply lt_wlt.
-
-      apply Nlt_out in H_chkr_snd.
-
-      repeat rewrite wordToN_to_nat in H_chkr_snd.
       apply H_chkr_snd.
       
     (* var = val *)
@@ -260,11 +251,6 @@ Qed.
       exists val2.
       repeat split; try reflexivity.
 
-      apply lt_wlt.
-
-      apply Nlt_out in H_chkr_snd.
-
-      repeat rewrite wordToN_to_nat in H_chkr_snd.
       apply H_chkr_snd.
 
     (* var = var *)
@@ -291,11 +277,6 @@ Qed.
       exists (model var2).
       repeat split; try reflexivity.
 
-      apply lt_wlt.
-
-      apply Nlt_out in H_chkr_snd.
-
-      repeat rewrite wordToN_to_nat in H_chkr_snd.
       apply H_chkr_snd.
 Qed.
 
@@ -338,5 +319,133 @@ Qed.
       unfold wlt in H_wlt_v2'_v1'.
       intuition.
 Qed.
+
+
+
+
   
+  Lemma chk_lt_wshift_wrt_ctx_snd:
+    forall ctx sv1 sv2 shift,
+      chk_lt_wshift_wrt_ctx ctx sv1 sv2 shift = true ->
+      forall model mem strg exts maxidx sb ops,
+        is_model (ctx_cs ctx) model = true ->
+        exists v1 v2,
+          eval_sstack_val sv1 model mem strg exts maxidx sb ops = Some v1 /\
+            eval_sstack_val sv2 model mem strg exts maxidx sb ops = Some v2 /\
+            ((wordToN v1)+shift < (wordToN v2))%N.
+    Proof.
+    intros ctx sv1 sv2 shift H_chkr model mem strg exts maxidx sb ops H_is_model.
+    destruct ctx as [cs [chkr H_chkr_snd]].
+    simpl in H_is_model.
+    
+    destruct sv1 as [val1 | var1 | fvar1 ] eqn:E_sv1;
+      destruct sv2 as [val2 | var2 | fvar2 ] eqn:E_sv2;
+      try discriminate.
+
+    (* val1 = val2 *)
+    + unfold chk_lt_wshift_wrt_ctx in H_chkr.
+      unfold sstack_val_to_cliteral in H_chkr.
+      unfold sstack_val_to_cliteral_w_shift in H_chkr.
+      simpl in H_chkr.
+      pose proof (H_chkr_snd cs (C_LT (C_VAL (wordToN val1 + shift)) (C_VAL (wordToN val2))) H_chkr) as H_chkr_snd.
+      unfold is_model in H_is_model.
+      unfold imply in H_chkr_snd.
+      pose proof (H_chkr_snd model H_is_model) as H_chkr_snd.
+      unfold satisfies_single_constraint in H_chkr_snd.
+      unfold cliteral_to_nat in H_chkr_snd.
+
+      pose proof (eval_sstack_val_Val val1 model mem strg exts maxidx sb ops) as H_eval_val1. 
+      pose proof (eval_sstack_val_Val val2 model mem strg exts maxidx sb ops) as H_eval_val2.
+
+
+      rewrite N.ltb_lt in H_chkr_snd.
+      
+      rewrite H_eval_val1.
+      rewrite H_eval_val2.
+
+      exists val1.
+      exists val2.
+      repeat split; try reflexivity.
+      apply H_chkr_snd.
+      
+    (* val = var *)
+    + unfold chk_lt_wshift_wrt_ctx in H_chkr.
+      unfold sstack_val_to_cliteral in H_chkr.
+      unfold sstack_val_to_cliteral_w_shift in H_chkr.
+      simpl in H_chkr.
+      pose proof (H_chkr_snd cs (C_LT (C_VAL (wordToN val1 + shift)) (C_VAR var2)) H_chkr) as H_chkr_snd.
+      unfold is_model in H_is_model.
+      unfold imply in H_chkr_snd.
+      pose proof (H_chkr_snd model H_is_model) as H_chkr_snd.
+      unfold satisfies_single_constraint in H_chkr_snd.
+      unfold cliteral_to_nat in H_chkr_snd.
+
+      pose proof (eval_sstack_val_Val val1 model mem strg exts maxidx sb ops) as H_eval_val1. 
+      pose proof (eval_sstack_val_InVar var2 model mem strg exts maxidx sb ops) as H_eval_var2.
+
+
+      rewrite N.ltb_lt in H_chkr_snd.
+      
+      rewrite H_eval_val1.
+      rewrite H_eval_var2.
+
+      exists val1.
+      exists (model var2).
+      repeat split; try reflexivity.
+      apply H_chkr_snd.
+      
+    (* var = val *)
+    + unfold chk_lt_wshift_wrt_ctx in H_chkr.
+      unfold sstack_val_to_cliteral in H_chkr.
+      unfold sstack_val_to_cliteral_w_shift in H_chkr.
+      simpl in H_chkr.
+      pose proof (H_chkr_snd cs (C_LT (C_VAR_DELTA var1 shift) (C_VAL (wordToN val2))) H_chkr) as H_chkr_snd.
+      unfold is_model in H_is_model.
+      unfold imply in H_chkr_snd.
+      pose proof (H_chkr_snd model H_is_model) as H_chkr_snd.
+      unfold satisfies_single_constraint in H_chkr_snd.
+      unfold cliteral_to_nat in H_chkr_snd.
+
+      pose proof (eval_sstack_val_InVar var1 model mem strg exts maxidx sb ops) as H_eval_var1.
+      pose proof (eval_sstack_val_Val val2 model mem strg exts maxidx sb ops) as H_eval_val2. 
+
+
+      rewrite N.ltb_lt in H_chkr_snd.
+      
+      rewrite H_eval_var1.
+      rewrite H_eval_val2.
+
+      exists (model var1).
+      exists val2.
+      repeat split; try reflexivity.
+      apply H_chkr_snd.
+
+    (* var = var *)
+    + unfold chk_lt_wshift_wrt_ctx in H_chkr.
+      unfold sstack_val_to_cliteral in H_chkr.
+      unfold sstack_val_to_cliteral_w_shift in H_chkr.
+      simpl in H_chkr.
+      pose proof (H_chkr_snd cs (C_LT (C_VAR_DELTA var1 shift) (C_VAR var2)) H_chkr) as H_chkr_snd.
+      unfold is_model in H_is_model.
+      unfold imply in H_chkr_snd.
+      pose proof (H_chkr_snd model H_is_model) as H_chkr_snd.
+      unfold satisfies_single_constraint in H_chkr_snd.
+      unfold cliteral_to_nat in H_chkr_snd.
+
+      pose proof (eval_sstack_val_InVar var1 model mem strg exts maxidx sb ops) as H_eval_var1.
+      pose proof (eval_sstack_val_InVar var2 model mem strg exts maxidx sb ops) as H_eval_var2. 
+
+
+      rewrite N.ltb_lt in H_chkr_snd.
+      
+      rewrite H_eval_var1.
+      rewrite H_eval_var2.
+
+      exists (model var1).
+      exists (model var2).
+      repeat split; try reflexivity.
+
+      apply H_chkr_snd.
+Qed.
+
 End ContextFacts.
